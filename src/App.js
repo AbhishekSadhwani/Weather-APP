e114f0218f42bbff444bde3cf902a31c75a7669d
Abhishek Sadhwani <abhisheksadhwani23@gmail.com>
2025-01-21 19:16:38 +0530

    Error Handling in Search form using react toast

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
+import { toast } from 'react-toastify';
 import {TodaysForecast, SearchComp, WeekForcast, DaysForcast, Loading} from './Components';
 import {SunCard} from './Components/Cards';
 import { useEffect, useState } from 'react';
 
 function App() {
+  const [city, setCity] = useState(localStorage.getItem("location") || 'London');
   const [weatherData, setWeatherData] = useState({});
   const [loading, setLoading] = useState(true);
-  const [searchQuery, setSearchQuery] = useState(localStorage.getItem("location") || 'London');
 
   // fetching weather data
   useEffect(() => {
     const fetchWeatherData = async () => {
       try{
         setLoading(true);
-        const response = await fetch(`${process.env.REACT_APP_WEATHER_BASE_URL}/forecast.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${searchQuery}&days=7&aqi=no&alerts=no`);
-        if(!response.ok){
-          throw new Error(response.statusText);
-        }
+        const response = await fetch(`${process.env.REACT_APP_WEATHER_BASE_URL}/forecast.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${city}&days=7&aqi=no&alerts=no`);
         
+        if(!response.ok){
+          toast("City not found. Please check the spelling.", {position:"top-right",style:{width:"250px",borderRadius:"10px"} ,closeButton:true, autoClose:3000});
+          throw new Error('City not found');
+        }
+
+
         const data = await response.json();
         setWeatherData(data);
-        localStorage.setItem("location", searchQuery);
+        localStorage.setItem("location", city);
       }
       catch(error){
         console.error('Error fetching weather data: ', error);
@@ -32,15 +36,12 @@ function App() {
 
     fetchWeatherData();
   
-  },[searchQuery]);
+  },[city]);
 
   // form to hanlde search query change
   const handleQueryChange = (query) => {
-    setSearchQuery(query);
+    setCity(query);
   };
-
-  console.log(weatherData);
-
 
   if(loading){
     return (
--- src/Components/SearchComp.js
+++ src/Components/SearchComp.js
@@ -1,11 +1,44 @@
 import { Search } from 'lucide-react'
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+
 
 export const SearchComp = ({handleQueryChange}) => {
+  // state to manage the toast position
+  const [toastPosition, setToastPosition] = useState("top-right");
 
+  // using useEffect to handle the toast position based on the window width
+  useEffect(() => {
+    const handleResize = () => {
+      setToastPosition(window.innerWidth < 768 ? "top-center" : "top-right");
+    };
+
+    window.addEventListener('resize', handleResize);
+    
+    // handling the position when the component is mounted 
+    handleResize();
+
+    // handling the position when the component is unmounted
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+
+  },[]);
+
+
+  // form to handle the search query change
   const handleFormSubmit = (e) => {
     e.preventDefault();
+
+    // checking the input value is empty
+    if(e.target.query.value.trim() === ''){
+      toast.error("Please enter a location to search", {position:toastPosition,style:{width:"250px",borderRadius:"10px"} ,closeButton:true, autoClose:3000});
+      return;
+    }
+
+    // passing the query to the parent component
     handleQueryChange(e.target.query.value);
+    // reset the input value
     e.target.query.value = '';
   }
 
